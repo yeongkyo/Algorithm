e710a65e8bb760ef6e2d2b81db1080d02a11c667
IN YEONG KYO <yeongkyo1997@gmail.com>
2025-11-23 11:53:01 +0900

    [Silver V] Title: Base Conversion, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11576. Base Conversion/Base Conversion.cc
+++ 백준/Silver/11576. Base Conversion/Base Conversion.cc
@@ -1,34 +1,37 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
 
 using namespace std;
 
 int main() {
-    int A, B, m;
-    cin >> A >> B;
-    cin >> m;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-    int decimal = 0;
-    vector<int> number(m);
+    int A, B;
+    if (cin >> A >> B) {
+        int m;
+        cin >> m;
 
-    for (int i = 0; i < m; i++) {
-        cin >> number[i];
+        long long decimalValue = 0;
+        for (int i = 0; i < m; ++i) {
+            int digit;
+            cin >> digit;
+            decimalValue = decimalValue * A + digit;
+        }
+
+        if (decimalValue == 0) {
+            cout << 0;
+        } else {
+            vector<int> result;
+            while (decimalValue > 0) {
+                result.push_back(decimalValue % B);
+                decimalValue /= B;
+            }
+
+            for (int i = result.size() - 1; i >= 0; --i) {
+                cout << result[i] << (i > 0 ? " " : "");
+            }
+        }
     }
-
-    for (int i = 0; i < m; i++) {
-        decimal += number[i] * pow(A, m - 1 - i);
-    }
-
-    vector<int> result;
-    while (decimal > 0) {
-        result.push_back(decimal % B);
-        decimal /= B;
-    }
-
-    for (int i = result.size() - 1; i >= 0; i--) {
-        cout << result[i] << ' ';
-    }
-
     return 0;
 }
