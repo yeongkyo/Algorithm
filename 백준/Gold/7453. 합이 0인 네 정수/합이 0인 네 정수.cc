31e772c18a43f16b5a51681712bad835b30f4f77
IN YEONG KYO <yeongkyo1997@gmail.com>
2026-02-23 09:37:28 +0900

    [Gold II] Title: 합이 0인 네 정수, Time: 6340 ms, Memory: 64664 KB -BaekjoonHub

--- 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.cc
+++ 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.cc
@@ -2,33 +2,36 @@
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false);
+    ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
+
     int n;
     cin >> n;
-    
+
     vector<int> A(n), B(n), C(n), D(n);
-    for (int i = 0; i < n; ++i)
+    for (int i = 0; i < n; i++) {
         cin >> A[i] >> B[i] >> C[i] >> D[i];
-    
-    vector<int> AB_sum;
-    AB_sum.reserve(n * n);
-    for (int a : A)
-        for (int b : B)
-            AB_sum.push_back(a + b);
-    
-    sort(AB_sum.begin(), AB_sum.end());
-    
-    long long cnt = 0;
-    for (int c : C) {
-        for (int d : D) {
-            int target = -(c + d);
-            auto range = equal_range(AB_sum.begin(), AB_sum.end(), target);
-            cnt += range.second - range.first;
+    }
+
+    vector<int> AB;
+    AB.reserve(1LL * n * n);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            AB.push_back(A[i] + B[j]);
         }
     }
-    
-    cout << cnt;
+
+    sort(AB.begin(), AB.end());
+
+    long long ans = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            int target = -(C[i] + D[j]);
+            auto range = equal_range(AB.begin(), AB.end(), target);
+            ans += (long long)(range.second - range.first);
+        }
+    }
+
+    cout << ans << "\n";
     return 0;
 }
