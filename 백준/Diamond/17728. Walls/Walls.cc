30dd02d2ac74b1f527c68795809274eb481e3836
IN YEONG KYO <yeongkyo1997@gmail.com>
2026-03-12 15:03:17 +0900

    [Diamond II] Title: Walls, Time: 24 ms, Memory: 5928 KB, Score: 10 point -BaekjoonHub

--- 백준/Diamond/17728. Walls/Walls.cc
+++ 백준/Diamond/17728. Walls/Walls.cc
@@ -2,14 +2,12 @@
 #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 alignas(32) int W[200008];
 alignas(32) int x[200008];
 alignas(32) long long ans[200008];
-int P[200008];
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -26,32 +24,47 @@ int main() {
         ans[i] = 0;
     }
     
+    vector<int> S;
+    S.reserve(M);
     for (int j = 0; j < M; ++j) {
-        cin >> P[j];
+        int p;
+        cin >> p;
+        if (S.empty()) {
+            S.push_back(p);
+            continue;
+        }
+        if (S.back() == p) continue;
+        
+        while (S.size() >= 2) {
+            long long a = S[S.size() - 2];
+            long long b = S.back();
+            if ((b - a > 0 && p - b >= 0) || (b - a < 0 && p - b <= 0)) {
+                S.pop_back();
+            } else {
+                break;
+            }
+        }
+        S.push_back(p);
     }
 
-    int N_pad = (N + 7) & ~7;
-    for(int i = N; i < N_pad; ++i) {
+    int N_pad = (N + 15) & ~15;
+    for (int i = N; i < N_pad; ++i) {
         W[i] = 0;
         x[i] = 0;
         ans[i] = 0;
     }
 
-    const int BLOCK_SIZE = 1024;
-    for (int i_start = 0; i_start < N_pad; i_start += BLOCK_SIZE) {
-        int i_end = min(N_pad, i_start + BLOCK_SIZE);
-        for (int j = 0; j < M; ++j) {
-            int p = P[j];
-            #pragma GCC ivdep
-            #pragma GCC unroll 8
-            for (int i = i_start; i < i_end; ++i) {
-                int cx = x[i];
-                int L = p - W[i];
-                int diff1 = max(0, L - cx);
-                int diff2 = max(0, cx - p);
-                ans[i] += diff1 + diff2;
-                x[i] = cx + diff1 - diff2;
-            }
+    int m_sz = S.size();
+    for (int j = 0; j < m_sz; ++j) {
+        int p = S[j];
+        #pragma GCC ivdep
+        for (int i = 0; i < N_pad; ++i) {
+            int cx = x[i];
+            int L = p - W[i];
+            int diff1 = L > cx ? L - cx : 0;
+            int diff2 = cx > p ? cx - p : 0;
+            ans[i] += diff1 + diff2;
+            x[i] = cx + diff1 - diff2;
         }
     }
 
