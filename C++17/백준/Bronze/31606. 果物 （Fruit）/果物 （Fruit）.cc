ef1efe215d7b3712b935ac823fe7a46345112c38
IN YEONG KYO <yeongkyo1997@gmail.com>
2026-03-23 07:19:08 +0900

    [Bronze V] Title: 果物 (Fruit), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/31606. 果物 （Fruit）/果物 （Fruit）.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int x, y;
+    cin >> x >> y;
+
+    cout << x + y + 3 << "\n";
+
+    return 0;
+}
