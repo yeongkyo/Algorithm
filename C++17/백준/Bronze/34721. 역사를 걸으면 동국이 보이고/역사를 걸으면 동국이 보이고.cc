6780dfe9488c86494707071c75cfa8cffc80acb3
IN YEONG KYO <yeongkyo1997@gmail.com>
2026-03-23 07:31:40 +0900

    [Bronze V] Title: 역사를 걸으면 동국이 보이고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/34721. 역사를 걸으면 동국이 보이고/역사를 걸으면 동국이 보이고.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    if (!(cin >> n)) return 0;
+
+    for (int i = 0; i < n; ++i) {
+        cout << "I love DGU" << "\n";
+    }
+
+    return 0;
+}
