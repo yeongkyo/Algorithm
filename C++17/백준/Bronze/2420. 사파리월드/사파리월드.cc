759ae71c519a1f96645908759f4a3e1bfb75853b
IN YEONG KYO <yeongkyo1997@gmail.com>
2026-03-27 14:37:07 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long long N, M;
+    cin >> N >> M;
+
+    cout << llabs(N - M);
+    return 0;
+}
