fc752fa5b2d8af5312c67112f8577bf04018d0b4
IN YEONG KYO <yeongkyo1997@gmail.com>
2026-03-27 14:25:24 +0900

    [Ruby IV] Title: Fibonacci representations, Time: 264 ms, Memory: 25468 KB, Score: 65 point -BaekjoonHub

--- C++17/백준/Ruby/16029. Fibonacci representations/Fibonacci representations.cc
+++ C++17/백준/Ruby/16029. Fibonacci representations/Fibonacci representations.cc
@@ -1,188 +1,177 @@
 #include <iostream>
+#include <vector>
+#include <random>
 
 using namespace std;
 
-const int MOD = 1000000007;
+const int MOD = 1e9 + 7;
 
 struct Matrix {
-    int mat[2][2];
+    long long mat[2][2];
     Matrix() {
         mat[0][0] = 1; mat[0][1] = 0;
         mat[1][0] = 0; mat[1][1] = 1;
     }
-    Matrix(int a, int b, int c, int d) {
-        mat[0][0] = a; mat[0][1] = b;
-        mat[1][0] = c; mat[1][1] = d;
+    Matrix(long long d) {
+        mat[0][0] = 1;
+        mat[0][1] = 1;
+        mat[1][0] = ((d - 1) / 2) % MOD;
+        mat[1][1] = (d / 2) % MOD;
     }
-    Matrix operator*(const Matrix& o) const {
-        return Matrix(
-            (1LL * mat[0][0] * o.mat[0][0] + 1LL * mat[0][1] * o.mat[1][0]) % MOD,
-            (1LL * mat[0][0] * o.mat[0][1] + 1LL * mat[0][1] * o.mat[1][1]) % MOD,
-            (1LL * mat[1][0] * o.mat[0][0] + 1LL * mat[1][1] * o.mat[1][0]) % MOD,
-            (1LL * mat[1][0] * o.mat[0][1] + 1LL * mat[1][1] * o.mat[1][1]) % MOD
-        );
+    Matrix operator*(const Matrix& other) const {
+        Matrix res;
+        res.mat[0][0] = (mat[0][0] * other.mat[0][0] + mat[0][1] * other.mat[1][0]) % MOD;
+        res.mat[0][1] = (mat[0][0] * other.mat[0][1] + mat[0][1] * other.mat[1][1]) % MOD;
+        res.mat[1][0] = (mat[1][0] * other.mat[0][0] + mat[1][1] * other.mat[1][0]) % MOD;
+        res.mat[1][1] = (mat[1][0] * other.mat[0][1] + mat[1][1] * other.mat[1][1]) % MOD;
+        return res;
     }
 };
-
-unsigned int xorshift() {
-    static unsigned int x = 123456789;
-    static unsigned int y = 362436069;
-    static unsigned int z = 521288629;
-    static unsigned int w = 88675123;
-    unsigned int t = x ^ (x << 11);
-    x = y; y = z; z = w;
-    return w = (w ^ (w >> 19)) ^ (t ^ (t >> 8));
-}
 
 struct Node {
-    int key;
-    unsigned int priority;
+    int val;
+    unsigned int pri;
+    int ls, rs;
     int min_val, max_val;
-    Matrix M;
-    int l, r;
-};
+    Matrix mat;
+} tree[400005];
 
-const int MAX_NODES = 5000005;
-Node pool[MAX_NODES];
-int node_cnt = 0;
-int trash[MAX_NODES];
-int trash_cnt = 0;
+int root = 0, node_cnt = 0;
+int free_list[400005], free_cnt = 0;
+mt19937 rng(1337);
 
-int allocNode(int key) {
-    int id = trash_cnt > 0 ? trash[--trash_cnt] : ++node_cnt;
-    pool[id].key = key;
-    pool[id].priority = xorshift();
-    pool[id].min_val = pool[id].max_val = key;
-    pool[id].M = Matrix();
-    pool[id].l = pool[id].r = 0;
-    return id;
+int new_node(int val) {
+    int u = free_cnt > 0 ? free_list[--free_cnt] : ++node_cnt;
+    tree[u].val = val;
+    tree[u].pri = rng();
+    tree[u].ls = tree[u].rs = 0;
+    tree[u].min_val = tree[u].max_val = val;
+    tree[u].mat = Matrix();
+    return u;
 }
 
-void freeNode(int id) {
-    if (id) trash[trash_cnt++] = id;
-}
-
-void update(int t) {
-    if (!t) return;
-    int l = pool[t].l;
-    int r = pool[t].r;
-
-    pool[t].min_val = l ? pool[l].min_val : pool[t].key;
-    pool[t].max_val = r ? pool[r].max_val : pool[t].key;
-
-    Matrix M(1, 0, 0, 1);
-    if (l) {
-        int d = pool[t].key - pool[l].max_val;
-        Matrix T(1, 1, (d - 1) / 2, d / 2);
-        M = T * pool[l].M;
+void push_up(int u) {
+    int ls = tree[u].ls;
+    int rs = tree[u].rs;
+    
+    tree[u].min_val = ls ? tree[ls].min_val : tree[u].val;
+    tree[u].max_val = rs ? tree[rs].max_val : tree[u].val;
+    
+    Matrix res; 
+    if (ls) {
+        res = tree[ls].mat;
+        res = Matrix(tree[u].val - tree[ls].max_val) * res;
     }
-    if (r) {
-        int d = pool[r].min_val - pool[t].key;
-        Matrix T(1, 1, (d - 1) / 2, d / 2);
-        M = pool[r].M * T * M;
+    if (rs) {
+        Matrix right_part = tree[rs].mat;
+        right_part = right_part * Matrix(tree[rs].min_val - tree[u].val);
+        res = right_part * res;
     }
-    pool[t].M = M;
+    tree[u].mat = res;
 }
 
-void split(int t, int key, int &l, int &r) {
-    if (!t) { l = r = 0; return; }
-    if (pool[t].key < key) {
-        split(pool[t].r, key, pool[t].r, r);
-        l = t;
+void split(int u, int val, int& x, int& y) {
+    if (!u) {
+        x = y = 0;
+        return;
+    }
+    if (tree[u].val <= val) {
+        x = u;
+        split(tree[u].rs, val, tree[u].rs, y);
     } else {
-        split(pool[t].l, key, l, pool[t].l);
-        r = t;
+        y = u;
+        split(tree[u].ls, val, x, tree[u].ls);
     }
-    update(t);
+    push_up(u);
 }
 
-void merge(int &t, int l, int r) {
-    if (!l || !r) { t = l ? l : r; return; }
-    if (pool[l].priority > pool[r].priority) {
-        merge(pool[l].r, pool[l].r, r);
-        t = l;
+int merge(int x, int y) {
+    if (!x || !y) return x ? x : y;
+    if (tree[x].pri > tree[y].pri) {
+        tree[x].rs = merge(tree[x].rs, y);
+        push_up(x);
+        return x;
     } else {
-        merge(pool[r].l, l, pool[r].l);
-        t = r;
+        tree[y].ls = merge(x, tree[y].ls);
+        push_up(y);
+        return y;
     }
-    update(t);
 }
 
-bool find(int t, int key) {
-    while (t) {
-        if (pool[t].key == key) return true;
-        if (pool[t].key < key) t = pool[t].r;
-        else t = pool[t].l;
+void insert_treap(int val) {
+    int x, y;
+    split(root, val, x, y);
+    root = merge(merge(x, new_node(val)), y);
+}
+
+void collect_free(int u) {
+    if (!u) return;
+    free_list[free_cnt++] = u;
+    collect_free(tree[u].ls);
+    collect_free(tree[u].rs);
+}
+
+void erase_treap(int val) {
+    int x, y, z;
+    split(root, val - 1, x, y);
+    split(y, val, y, z);
+    collect_free(y);
+    root = merge(x, z);
+}
+
+bool contains(int val) {
+    int u = root;
+    while (u) {
+        if (tree[u].val == val) return true;
+        if (val < tree[u].val) u = tree[u].ls;
+        else u = tree[u].rs;
     }
     return false;
 }
 
-void insert(int &t, int key) {
-    int l, r;
-    split(t, key, l, r);
-    int mid = allocNode(key);
-    merge(t, l, mid);
-    merge(t, t, r);
+void add(int x) {
+    if (x <= 0) return;
+    if (contains(x)) {
+        erase_treap(x);
+        add(x + 1);
+        if (x == 1) ;
+        else if (x == 2) add(1);
+        else add(x - 2);
+        return;
+    }
+    if (contains(x + 1)) {
+        erase_treap(x + 1);
+        add(x + 2);
+        return;
+    }
+    if (contains(x - 1)) {
+        erase_treap(x - 1);
+        add(x + 1);
+        return;
+    }
+    insert_treap(x);
 }
 
-void erase(int &t, int key) {
-    int l, mid, r;
-    split(t, key, l, r);
-    split(r, key + 1, mid, r);
-    freeNode(mid);
-    merge(t, l, r);
-}
-
-int root = 0;
-
-void add(int v) {
-    if (find(root, v)) {
-        erase(root, v);
-        if (v == 1) {
-            add(2);
-        } else if (v == 2) {
-            add(3);
-            add(1);
-        } else {
-            add(v + 1);
-            add(v - 2);
-        }
-        return;
-    }
-    if (find(root, v - 1)) {
-        erase(root, v - 1);
-        add(v + 1);
-        return;
-    }
-    if (find(root, v + 1)) {
-        erase(root, v + 1);
-        add(v + 2);
-        return;
-    }
-    insert(root, v);
+long long get_ans() {
+    if (!root) return 0;
+    Matrix total = tree[root].mat;
+    Matrix first_gap(tree[root].min_val);
+    total = total * first_gap;
+    long long ans = (total.mat[0][0] + total.mat[1][0]) % MOD;
+    return ans;
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
+    
     int n;
     if (!(cin >> n)) return 0;
-
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; ++i) {
         int a;
         cin >> a;
         add(a);
-
-        if (!root) {
-            cout << 0 << "\n";
-        } else {
-            int c1 = pool[root].min_val;
-            Matrix T(1, 1, (c1 - 1) / 2, c1 / 2);
-            Matrix M_final = pool[root].M * T;
-            int ans = (M_final.mat[0][0] + M_final.mat[1][0]) % MOD;
-            cout << ans << "\n";
-        }
+        cout << get_ans() << "\n";
     }
     return 0;
 }
