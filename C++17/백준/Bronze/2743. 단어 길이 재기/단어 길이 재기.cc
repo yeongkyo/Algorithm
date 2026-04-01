dd0b7eef05875251d5420a417a2d44730c77db94
IN YEONG KYO <yeongkyo1997@gmail.com>
2026-04-02 07:20:55 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string s;
+    cin >> s;
+
+    cout << s.length() << "\n";
+
+    return 0;
+}
