2bc21d54b49b67cd10c636269631cda9d18d9dbe
IN YEONG KYO <yeongkyo1997@gmail.com>
2026-04-02 07:23:35 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int R1, S;
+    
+    // R1과 S를 입력받음
+    cin >> R1 >> S;
+    
+    // R2 = 2 * S - R1 공식을 이용하여 결과 출력
+    cout << 2 * S - R1 << endl;
+    
+    return 0;
+}
