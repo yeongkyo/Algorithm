5d6a7eebc7f0e7f73466aef7e04d59be14b74b14
IN YEONG KYO <yeongkyo1997@gmail.com>
2026-04-02 07:23:10 +0900

    [Bronze V] Title: 등록, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/7287. 등록/등록.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 첫 줄에 맞은 문제 수 출력
+    cout << 441 << endl;
+    
+    // 둘째 줄에 아이디 출력
+    cout << "dmseka0829" << endl;
+
+    return 0;
+}
