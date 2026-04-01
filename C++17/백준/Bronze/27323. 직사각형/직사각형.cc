718c73c257916d9004ab7a30ca485fad72f2a9a2
IN YEONG KYO <yeongkyo1997@gmail.com>
2026-04-02 07:26:35 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int A, B;
+
+    // 세로 길이 A와 가로 길이 B를 입력받음
+    cin >> A >> B;
+
+    // 넓이(A * B)를 계산하여 출력
+    cout << A * B << endl;
+
+    return 0;
+}
